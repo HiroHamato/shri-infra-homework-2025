53553fe093f7ea48e5f3dddca2269e8a394b17ba
HiroHamato <72993255+HiroHamato@users.noreply.github.com>
2025-07-11 00:33:43 +0300

    Update About.tsx

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p className="about__paragraph">
-            Владислав
+            Влад
           </p>
         </div>
       </div>
